7acdf1127616d30df70b3fa901cad5ff35840669
luizgdias <gusttavodiias@gmail.com>
2022-03-03 17:56:09 -0300

    clean wf.ph

--- sources/wf.py
+++ sources/wf.py
@@ -4,16 +4,4 @@ from extractConfigParam import *
 
 print("++++++++++++++")
 
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/remove_pipe.py -f ' + validation_input)
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/mafft.py -f '+ alignment_input)
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/mrbayes.py -f '+ alignment_input + " -nr "+ nruns + " -nc " + nchains + " -brn " +burnin)
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/remove_pipe.py -f File1023')
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/mafft.py -f Validation_output')
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/model_generator.py -f file1024')
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/read_seq.py -f file1024')
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/mrbayes.py -f fileConvertedAlignment -f fileEvolutiveModel -nr nruns {} -nc nchains {} -brn burnin {} -prt printfreq {} -ng ngen {} -rt rates_mrbayes {}nrunsnchainsburninprintfreqngenrates_mrbayes')
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/remove_pipe.py -f File1023')
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/mafft.py -f Validation_output')
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/model_generator.py -f file1024')
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/read_seq.py -f file1024')
-os.system('/home/luiz/PycharmProjects/MaestroOO/sources/mrbayes.py -f fileConvertedAlignment -f fileEvolutiveModel -nr nruns -nc nchains -brn burnin -prt printfreq -ng ngen -rt rates_mrbayesnrunsnchainsburninprintfreqngenrates_mrbayes')
+
